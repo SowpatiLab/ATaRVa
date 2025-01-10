54bb2275cd2177a7c2f85d10255ac12fe49cd858
Dhaksnamoorthy <abisheakkumar141@gmail.com>
2025-01-10 22:06:53 +0530

    Added variables to process and store flank insertions

--- ATARVA/cstag_utils.py
+++ ATARVA/cstag_utils.py
@@ -3,7 +3,7 @@ from operation_utils import match_jump, deletion_jump, insertion_jump
 
 
 def parse_cstag(read_index, cs_tag, read_start, loci_keys, loci_coords, read_loci_variations,
-                homopoly_positions, global_read_variations, global_snp_positions, read_sequence, read_quality, cigar_one, sorted_global_snp_list):
+                homopoly_positions, global_read_variations, global_snp_positions, read_sequence, read_quality, cigar_one, sorted_global_snp_list, left_flank_list, right_flank_list):
     """
     Parse the CS tag for a read and record the variations observed for the read also for the loci
     """
@@ -14,11 +14,23 @@ def parse_cstag(read_index, cs_tag, read_start, loci_keys, loci_coords, read_loc
     qpos = 0            # starts from 0 the sub string the read sequence in python
 
     locus_qpos_range = []
+    loci_flank_qpos_range = []
+    out_insertion_qpos_ranges_left = []
+    out_insertion_qpos_ranges_right = []
+    left_ins_rpos = []
+    right_ins_rpos = []
     for _ in loci_coords:
         locus_qpos_range.append([0,0])
+        loci_flank_qpos_range.append([0,0])
+        out_insertion_qpos_ranges_left.append([])
+        out_insertion_qpos_ranges_right.append([])
+        left_ins_rpos.append([])
+        right_ins_rpos.append([])
 
     repeat_index = 0
-    tracked = [False] * len(loci_coords)
+    tracked = [False] * len(loci_coords)        
+
+    flank_track = [[False,False] for _ in loci_coords]
 
     if cigar_one[0] == 4:
         qpos+=cigar_one[1]
@@ -33,7 +45,7 @@ def parse_cstag(read_index, cs_tag, read_start, loci_keys, loci_coords, read_loc
 
             match_len = int(match_len)
             qpos += match_len; rpos += match_len
-            repeat_index += match_jump(rpos, repeat_index, loci_coords, tracked, locus_qpos_range, qpos, match_len)
+            repeat_index += match_jump(rpos, repeat_index, loci_coords, tracked, locus_qpos_range, qpos, match_len, loci_flank_qpos_range, flank_track, left_flank_list, right_flank_list)
 
         elif cs_tag[i] == '=':      # sequence match in long CS is followed by nucs which are matching       
             match_len = 0
@@ -41,7 +53,7 @@ def parse_cstag(read_index, cs_tag, read_start, loci_keys, loci_coords, read_loc
                 match_len += 1; i += 1
 
             qpos += match_len; rpos += match_len
-            repeat_index += match_jump(rpos, repeat_index, loci_coords, tracked, locus_qpos_range, qpos, match_len)
+            repeat_index += match_jump(rpos, repeat_index, loci_coords, tracked, locus_qpos_range, qpos, match_len, loci_flank_qpos_range, flank_track, left_flank_list, right_flank_list)
 
         elif cs_tag[i] == '*':      # substitution of a base; is followed by reference and substituted base
             ref_nuc = cs_tag[i+1]; sub_nuc = cs_tag[i+2]
@@ -62,7 +74,7 @@ def parse_cstag(read_index, cs_tag, read_start, loci_keys, loci_coords, read_loc
                 else: global_snp_positions[rpos][sub_nuc] = {read_index}
             
             qpos += 1; rpos += 1; match_len = 1
-            repeat_index += match_jump(rpos, repeat_index, loci_coords, tracked, locus_qpos_range, qpos, match_len)
+            repeat_index += match_jump(rpos, repeat_index, loci_coords, tracked, locus_qpos_range, qpos, match_len, loci_flank_qpos_range, flank_track, left_flank_list, right_flank_list)
 
         elif cs_tag[i] == '+':      # insertion; is followed by the inserted bases
             insert = ''; insert_length = 0; i += 1
@@ -71,7 +83,7 @@ def parse_cstag(read_index, cs_tag, read_start, loci_keys, loci_coords, read_loc
                 i += 1
             qpos += insert_length
             repeat_index += insertion_jump(insert_length, insert, rpos, repeat_index, loci_keys,
-                                           tracked, loci_coords, homopoly_positions, read_loci_variations, locus_qpos_range, qpos)
+                                           tracked, loci_coords, homopoly_positions, read_loci_variations, locus_qpos_range, qpos, loci_flank_qpos_range, flank_track, left_flank_list, right_flank_list, out_insertion_qpos_ranges_left, out_insertion_qpos_ranges_right, left_ins_rpos, right_ins_rpos)
 
         elif cs_tag[i] == '-':      # deletion; is followed by the deleted bases
             deletion = ''; deletion_length = 0; i += 1
@@ -81,9 +93,14 @@ def parse_cstag(read_index, cs_tag, read_start, loci_keys, loci_coords, read_loc
             # for _ in range(deletion_length): global_read_variations[read_index]['dels'].add(rpos+_)
             rpos += deletion_length
             repeat_index += deletion_jump(deletion_length, rpos, repeat_index, loci_keys, tracked, loci_coords,
-                                          homopoly_positions, read_loci_variations, locus_qpos_range, qpos)
+                                          homopoly_positions, read_loci_variations, locus_qpos_range, qpos, loci_flank_qpos_range, flank_track, left_flank_list, right_flank_list)
             
     for idx,each_key in enumerate(loci_keys):
         s_pos = locus_qpos_range[idx][0]
         e_pos = locus_qpos_range[idx][1]
-        read_loci_variations[each_key]['seq'] = read_sequence[s_pos:e_pos]
+        
+        loci_flank_qpos_range[idx][0] = loci_flank_qpos_range[idx][0] - s_pos
+        loci_flank_qpos_range[idx][1] = loci_flank_qpos_range[idx][1] - s_pos
+        ins_left = [(each_tuple[0]-s_pos, each_tuple[1]-s_pos) for each_tuple in out_insertion_qpos_ranges_left[idx]]
+        ins_right = [(each_tuple[0]-s_pos, each_tuple[1]-s_pos) for each_tuple in out_insertion_qpos_ranges_right[idx]]
+        read_loci_variations[each_key]['seq'] = [read_sequence[s_pos:e_pos], loci_flank_qpos_range[idx], ins_left, ins_right, left_ins_rpos[idx], right_ins_rpos[idx]]
