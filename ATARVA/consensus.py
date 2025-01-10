654ff20d690929d55185785e1a4b31b47b264720
Dhaksnamoorthy <abisheakkumar141@gmail.com>
2025-01-10 19:25:52 +0530

    Changed consensus method to pyabpoa

--- ATARVA/consensus.py
+++ ATARVA/consensus.py
@@ -1,22 +1,37 @@
+import pyabpoa as pa
 import sys
 import statistics
 
 def consensus_seq_poa(seqs, alen):
-
-    tot_seq = len(seqs)
-    seq_len = len(seqs[0])
-    
-    consensus = ''
-    for idx in range(seq_len):
-        current_base = []
-        for each_seq in seqs:
-            current_base.append(each_seq[idx])
-        consensus+=statistics.mode(current_base)
-
-    if len(consensus)!=alen:
-        print('Error in the Consensus!!!')
-        print('consensus_len = ', len(consensus), 'alen = ', alen)
-        print(seqs)
-        sys.exit()
+    if len(seqs)<7:
+        cons_algrm='MF'
     else:
-        return consensus
+        cons_algrm='HB'
+
+    abpoa = pa.msa_aligner(cons_algrm=cons_algrm)
+    result = abpoa.msa(seqs, out_cons=True, out_msa=False)
+    return result.cons_seq[0]
+
+    # if len(seq)==alen:
+    #     return seq
+    # else:
+    #     print('Error in consensus')
+    #     sys.exit()
+
+    # tot_seq = len(seqs)
+    # seq_len = len(seqs[0])
+    
+    # consensus = ''
+    # for idx in range(seq_len):
+    #     current_base = []
+    #     for each_seq in seqs:
+    #         current_base.append(each_seq[idx])
+    #     consensus+=statistics.mode(current_base)
+
+    # if len(consensus)!=alen:
+    #     print('Error in the Consensus!!!')
+    #     print('consensus_len = ', len(consensus), 'alen = ', alen)
+    #     print(seqs)
+    #     sys.exit()
+    # else:
+    #     return consensus
