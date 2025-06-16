3733109eb4197c3cbe43afe3c72bc30928d5334a
Dhaksnamoorthy <abisheakkumar141@gmail.com>
2025-06-16 18:23:58 +0530

    cyclic variation

--- ATARVA/decomp_utils.py
+++ ATARVA/decomp_utils.py
@@ -58,7 +58,7 @@ def kmp_search_non_overlapping(text, pattern):
 
     lps = compute_lps(pattern)
     result = []
-    i = 0  
+    i = 0
     j = 0  
 
     while i < len(text):
@@ -77,26 +77,38 @@ def kmp_search_non_overlapping(text, pattern):
 
     return result
 
-def get_most_frequent_motif(sequence, motif_size):
+def get_most_frequent_motif(sequence, motif_size, motif):
+    cyc_motif = get_canonical_motif(motif) if motif else None
     if len(sequence) < motif_size:  
         return sequence, None  
 
     repeating_units = [sequence[i:i + motif_size] for i in range(len(sequence) - motif_size + 1)]
     motif_counts = Counter(repeating_units)
     
+    
     if not motif_counts:  
         return sequence, None  
 
-    most_common = motif_counts.most_common(2) 
-    
-    primary_motif = most_common[0][0] if most_common else sequence
-    secondary_motif = most_common[1][0] if len(most_common) > 1 else None
+    most_common = []
+    for each_motif,count in motif_counts.most_common():
+        if len(most_common) >=2: break
+        if get_canonical_motif(each_motif) == cyc_motif:
+            if motif not in most_common: most_common.append(motif)
+        else:
+            most_common.append(each_motif)
+
+    primary_motif = most_common[0] if most_common else sequence
+    secondary_motif = most_common[1] if len(most_common) > 1 else None
 
     return primary_motif, secondary_motif
+    
+def get_canonical_motif(motif):
+    return min(motif[i:] + motif[:i] for i in range(len(motif)))
 
-def motif_decomposition(sequence, motif_size):
-    best_motif_size = min(motif_size , shift_and_match(sequence, motif_size))  
-    primary_motif, secondary_motif = get_most_frequent_motif(sequence, best_motif_size)
+def motif_decomposition(sequence, motif_size, motif):
+
+    best_motif_size = min(motif_size , shift_and_match(sequence, motif_size)) 
+    primary_motif, secondary_motif = get_most_frequent_motif(sequence, best_motif_size, motif)
     
     positions = kmp_search_non_overlapping(sequence, primary_motif)
     
@@ -108,6 +120,14 @@ def motif_decomposition(sequence, motif_size):
 
     # if positions[0] != 0:
     #     decomposed_parts.append(sequence[:positions[0]])  
+
+    prefix = sequence[:positions[0]]
+    if len(prefix) >= motif_size * 2:
+        secondary_motif_size = shift_and_match(prefix, motif_size)
+        secondary_decomposition = motif_decomposition(prefix, secondary_motif_size, '')
+        decomposed_parts.append(secondary_decomposition)
+    elif prefix:
+        decomposed_parts.append(prefix)
     
     for i in range(1, len(positions)):
         if positions[i] == positions[i - 1] + len(primary_motif):
@@ -118,7 +138,7 @@ def motif_decomposition(sequence, motif_size):
             if interspersed:
                 if len(interspersed) >= motif_size * 2:
                     secondary_motif_size = shift_and_match(interspersed, motif_size)
-                    secondary_decomposition = motif_decomposition(interspersed, secondary_motif_size)
+                    secondary_decomposition = motif_decomposition(interspersed, secondary_motif_size, '')
                     decomposed_parts.append(secondary_decomposition)
                 else:
                     decomposed_parts.append(interspersed)
@@ -131,9 +151,9 @@ def motif_decomposition(sequence, motif_size):
     if leftover_sequence:
         if len(leftover_sequence) >= motif_size * 2:
             secondary_motif_size = shift_and_match(leftover_sequence, motif_size)
-            secondary_decomposition = motif_decomposition(leftover_sequence, secondary_motif_size)
+            secondary_decomposition = motif_decomposition(leftover_sequence, secondary_motif_size, '')
             decomposed_parts.append(secondary_decomposition)
         else:
             decomposed_parts.append(leftover_sequence)
-    
+
     return "-".join(decomposed_parts)
